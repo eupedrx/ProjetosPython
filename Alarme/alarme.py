918e1f2cbcbd71b4e47b154aee61c1129cf4e106
satestagiario <pedro1saturno@gmail.com>
2024-08-27 12:22:32 -0300

    desenvolvida a estrutura inicial para o alarme

---
+++ Alarme/alarme.py
@@ -0,0 +1,18 @@
+import time
+import playsound
+
+def set_alarm(alarm_time):
+    print(f"Alarme definido para {alarm_time}")
+
+    while True:
+        current_time = time.strftime("%H:%M:%S")
+        if current_time == alarm_time:
+            print("Acorda corno!")
+            playsound("alarme.mp3")
+            break
+            time.sleep(2)
+
+
+alarm_time = input("Defina a hora do alarme (HH:MM:SS): ")
+set_alarm(alarm_time)
+
